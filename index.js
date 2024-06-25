a6781ca7404ed388c2f95a99883cff475e2cbae3
Wealth <shuaibganiyat20@gmail.com>
2024-06-25 13:46:09 +0100

    Added loader

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const numArticle = document.querySelector('#num')
 const advArticle = document.querySelector('#article')
 const newAdviceBtn = document.querySelector('#icon')
+const loader = document.getElementById("loader");
 
 const getAdvice = () => {
+    loader.style.display = "block";  
+    advArticle.style.display = "none";  
 const url = "https://api.adviceslip.com/advice"
 fetch(url)
 .then((response) => response.json())
 .then((data) => {
-
+loader.style.display = "none"; 
+advArticle.style.display = "inline";  
 numArticle.innerHTML = `${data.slip.id}`;
 advArticle.innerHTML = `${data.slip.advice}`;
 })
 .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
+    loader.style.display = "none";  
+    advArticle.style.display = "inline";  
+    advArticle.textContent = "An error occurred. Please try again.";
 });
 };
 newAdviceBtn.addEventListener("click", getAdvice)
+getAdvice();
